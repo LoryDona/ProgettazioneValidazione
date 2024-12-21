6c8ce94339dd906d5141e557d6cef51387f86d3d
Marjo1996 <marjo_95@hotmail.it>
2024-12-21 23:59:06 +0100

    Sviluppo primo scenario Mario

--- esameProgValid/src/main/java/demo/AppController.java
+++ esameProgValid/src/main/java/demo/AppController.java
@@ -2,17 +2,15 @@ package demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import javax.mail.*;
 import javax.mail.internet.*;
-import java.util.Properties;
-
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Controller
 public class AppController {
@@ -29,15 +27,35 @@ public class AppController {
     @PostMapping("/credenziali")
     public String login(@RequestParam("username") String username,
                         @RequestParam("password") String password,
-                        @RequestParam("ruolo") String ruolo,
+                        @RequestParam("role") String role,
                         @RequestParam("mail") String mail, Model model) {
 
-        if ("mario".equals(username)  && "rossi".equals(password) && "amministrativo".equals(ruolo) && "mail@gmail.com".equals(mail)) {
-            return "list"; // redirect alla list
-        } else {
-            model.addAttribute("error", "Username o password errati");
-            return "login"; // torna alla pagina di login
+        String[] parts = username.split(" ");
+        String firstName = parts.length > 0 ? parts[0] : "";
+        String lastName = parts.length > 1 ? parts[1] : "";
+
+        for (Person p: repository.findAll()) {
+            {
+                // Esiste già l'utente, non possono esserci 2 username uguali
+                if (p.getFirstName().equals(firstName) && p.getLastName().equals(lastName)) {
+                    return "userAlreadyExists";
+                }
+            }
         }
+
+        if(role.equals("administrator")) {
+            repository.save(new Administrator(firstName, lastName, password));
+        }
+        else if(role.equals("scientificManager"))
+        {
+            repository.save(new ScientificManager(firstName, lastName, password));
+        }
+        else
+        {
+            repository.save(new Researcher(firstName, lastName, password));
+        }
+
+        return "login";
     }
 
     // Metodo per gestire il login
@@ -79,8 +97,10 @@ public class AppController {
     @RequestMapping("/create")
     public String create(
             @RequestParam(name="firstname", required=true) String firstname,
-            @RequestParam(name="lastname", required=true) String lastname) {
-        repository.save(new Person(firstname,lastname));
+            @RequestParam(name="lastname", required=true) String lastname,
+            @RequestParam(name="password", required=true) String password,
+            @RequestParam(name="role", required=true) String role) {
+        repository.save(new Person(firstname,lastname, password, role));
         return "redirect:/list";
     }
 
@@ -103,11 +123,13 @@ public class AppController {
             @RequestParam(name="id", required=true) Long id,
             @RequestParam(name="firstname", required=true) String firstname,
             @RequestParam(name="lastname", required=true) String lastname,
+            @RequestParam(name="password", required=true) String password,
             Model model) {
         Optional<Person> result = repository.findById(id);
         if (result.isPresent()) {
+            String role = result.get().getRole();
             repository.delete(result.get());
-            Person person = new Person(firstname,lastname);
+            Person person = new Person(firstname,lastname, password, role);
             repository.save(person);
             return "redirect:/list";
         }
@@ -127,7 +149,48 @@ public class AppController {
             return "notfound";
     }
 
-    @RequestMapping("/listProjects")
+    @RequestMapping("/mainPage")
+    public String mainPage(@RequestParam(name="username", required=true) String username,
+                               @RequestParam(name="password", required=true) String password, Model model) {
+
+        Optional<Person> result = null;
+
+        String[] parts = username.split(" ");
+        String firstName = parts.length > 0 ? parts[0] : "";
+        String lastName = parts.length > 1 ? parts[1] : "";
+
+        for (Person p: repository.findAll()) {
+            {
+                // Esiste già l'utente, non possono esserci 2 username uguali
+
+                if (p.getFirstName().equals(firstName) && p.getLastName().equals(lastName)
+                    && p.getPassword().equals(password)) {
+                    result = repository.findById(p.getId());
+                    break;
+                }
+            }
+        }
+
+
+        if(result != null && result.isPresent()) {
+            if (result.get() instanceof Administrator) {
+                model.addAttribute("person", (Administrator) result.get());
+
+                return "pageAdministrator";
+            } else if (result.get() instanceof ScientificManager) {
+                model.addAttribute("person", (ScientificManager) result.get());
+
+                return "pageScientificManager";
+            } else {
+                model.addAttribute("person", (Researcher) result.get());
+                return "pageResearcher"; // Da implementare ancora
+            }
+        }
+
+        return "_error";
+    }
+
+        @RequestMapping("/listProjects")
     public String listProjects(Model model)
     {
 
@@ -137,4 +200,65 @@ public class AppController {
         return "projects";
     }
 
+    @RequestMapping("/createUser")
+    public String createUser(Model model)
+    {
+
+        return "createUser";
+    }
+
+    @RequestMapping("/createProjectData")
+    public String createProjectData(@RequestParam(name="IDAdministrator", required=true) Long IDAdministrator,
+                                    Model model)
+    {
+
+        model.addAttribute("administrator", repository.findById(IDAdministrator).get());
+
+        model.addAttribute("scientificManager", repository.findByRole("scientificManager"));
+
+
+
+        return "createProject";
+    }
+
+    @PostMapping("/createProject")
+    public String createProject(
+            @RequestParam(name = "nameProject", required = true) String nameProject,
+            @RequestParam(name = "administratorName", required = true) String administratorName,
+            @RequestParam(name = "administrator.getId()", required = true) Long IDAdministrator,
+            @RequestParam(name = "scientificManager.getId()", required = true) Long scientificManagerId,
+            @RequestParam(name = "state", required = true) String state,
+            @RequestParam(name = "startDate", required = true) String startDate,
+            @RequestParam(name = "endDate", required = true) String endDate,
+            @RequestParam(name = "budget", required = true) Double budget,
+            Model model) {
+        // Implementazione del metodo
+        Administrator admin = (Administrator) repository.findById(IDAdministrator).get();
+        ScientificManager scientificManager = (ScientificManager) repository.findById(scientificManagerId).get();
+
+
+        admin.addProject(nameProject, scientificManager, state, java.sql.Date.valueOf(startDate), java.sql.Date.valueOf(endDate), budget.intValue());
+
+        model.addAttribute("person", (Administrator) admin);
+        return "pageAdministrator";
+    }
+
+    @GetMapping("/listProjectScientificManager")
+    public String listProjectScientificManager(
+            @RequestParam("IDScientificManager") Long idScientificManager,
+            Model model) {
+
+        ScientificManager scientificManager = (ScientificManager) repository.findById(idScientificManager).get();
+        List<Project> projectsScientificManager = new ArrayList<>();
+
+        for (Project p : Administrator.projects) {
+            if(p.getScientificManager().getId().equals(idScientificManager)) {
+                projectsScientificManager.add(p);
+            }
+        }
+
+        model.addAttribute("projects", projectsScientificManager);
+
+        return "projects";
+    }
 }
